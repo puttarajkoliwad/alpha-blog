e7167b0b8c6656358c0e70e3b377ad15a7dc09a2
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-09-28 19:44:10 +0530

    mvc created for DB with show&index actions

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+#In order to communicate with the our 'articles' table, we need to create a model('article.rb') for it.
+class Article < ApplicationRecord
+
+#Model file for database is used to enforce constraints/validations for our tables.
+# validates :<column_name>, <constraint1>:<constraint value>, <constraint2>:<constraint value>
+    validates :title, presence: true, length: {minimum: 3, maximum:100}
+    validates :description, presence:true, length:{minimum: 6, maximum: 150}
+
+  #index: index is a special route action/method that lists all the entries of a database table.
+  #show: show is another special action/ method that shows any selected entry from the table.
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   #If webpacker doesn't run, try running "rails webpacker:install"
+
+  # rails routes --expanded : This command lists all the routes of the application.
   
   #Home page
   root "pages#home"
   
   #About page
   get 'about', to:"pages#about"
+
+  #Routes for resources can be generated with simple syntax: resources :articles
+  resources :articles, only:[:show, :index]
+
+  #index: index is a special method that lists all the entries of a database table.
+  #show: show is a method that shows any selected entry from the table.
 end
