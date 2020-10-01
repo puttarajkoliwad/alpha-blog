64798369e7978f12333e6e4b67e870e650f3bb41
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-10-02 01:45:10 +0530

    Completed ALPHA-BLOG!

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
 #Model file for database is used to enforce constraints/validations for our tables.
 # validates :<column_name>, <constraint1>:<constraint value>, <constraint2>:<constraint value>
     validates :title, presence: true, length: {minimum: 3, maximum:100}
-    validates :description, presence:true, length:{minimum: 6, maximum: 150}
+    validates :description, presence:true, length:{minimum: 6}
 
   #index: index is a special route action/method that lists all the entries of a database table.
   #show: show is another special action/ method that shows any selected entry from the table.
