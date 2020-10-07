a3b8ede4949210fc7752b8c6214bbcc599c1eb60
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-10-07 22:43:47 +0530

    Edit user details through front-end complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,28 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "User details updated successfully!"
+            redirect_to articles_path
+        else
+            render "edit"
+        end
+    end
+
 
 
 
