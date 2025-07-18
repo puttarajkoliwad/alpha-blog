d90dc46ea40a0f3a9a4ac820d5eb50c628a27578
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2025-07-18 16:48:14 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create a non-admin system user
+user = User.new(username: 'system', email: 'system@alphablog.com', admin: false)
+user.save(validate: false)
+
+# create an article
+articles = [
+  {
+    "title"=>"Hope",
+    "description"=> "\"Hope\" is the thing with feathers\r\nThat perches in the soul,\r\nAnd sings the tune without the words,\r\nAnd never stops at all.\r\n\r\nAnd sweetest in the gale is heard;\r\nAnd sore must be the storm\r\nThat could abash the little bird\r\nThat kept so many warm.\r\n\r\nI've heard it in the chilliest land\r\nAnd on the strangest sea;\r\nYet, never, in extremity,\r\nIt asked a crumb of me.\r\n\r\n- EMILY DICKINSON",
+    "user_id"=>user.id
+  }
+]
+
+articles.each do |article|
+  a = Article.new(article)
+  if a.save
+    puts "Article '#{a.title}' created successfully."
+  else
+    puts "Error creating article: #{a.errors.full_messages.join(', ')}"
+  end
+end
